60910db0f2886ec74f8b9aa23b97056f4bc5639b
George Ravelly da Silva Ribeiro <49557536+George1050@users.noreply.github.com>
2019-06-13 19:06:20 -0300

    Create dungeon.c
    Arquivo principal do jogo.

---
+++ dungeon.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "heroi.h"
+#include "dungeon.h"
+
+
+void tela(){
+
+	
+}
+//ATRIBUI OS PONTOS DE HABILIDADE
+
+void load(){
+	for(int i = 0; i<5;i++){
+		printf("Load.");
+		system("cls");
+		printf("Load..");
+		system("cls");
+		printf("Load...");
+		system("cls");
+	}
+}
+int main(){
+	int escolha = 0;
+	do{
+		printf("\n\t\tLABIRINTIA\n========================================\n");
+		printf(" 1.Iniciar \t2.Sobre \t3.Sair\n>>");
+		scanf("%d",&escolha);
+		setbuf(stdin,NULL);
+		if(escolha == 1){
+			cadHeroi();
+			printf("Se inicia a sua jornada %s",nome);
+			system("pause");
+			do{
+				if(andar == 1){
+					load();
+					andar = lab();
+				}else if(andar == 2){
+					load();
+					andar = lab();
+				}else if(andar == 3){
+					load();
+					andar = lab();
+				}else{
+					printf("\tObrigado por Jogar!!!\n");
+					escolha = 3;
+				}
+			}while(andar != 4);
+		}else if(escolha == 2){
+			system("cls");
+			printf("\nEste jogo foi criado por George Ravelly da Silva Ribeiro!!!\n");
+			system("pause");
+			system("cls");
+		}else if(escolha == 3){
+			printf("\tObrigado pela sua atencao!!!\n");
+		}else{
+			printf("Opcao Invalida");
+		}
+	}while(escolha!=3);
+	return 0;
+}
